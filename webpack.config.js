2793c3ae8e259b4f6ae6fbb376899b6bb40a56ec
FridayDai <friday.dai@xplusz.com>
2016-07-12 08:32:35 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
 var path = require('path');
-var webpack = require('webpack');
+//var webpack = require('webpack');
 
+
+//use webpack
+// module.exports = {
+//   entry: './minus.js',
+//   output: { path: __dirname, filename: 'bundle.js' },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\js|jsx$/,
+//         loader: 'babel-loader',
+//         exclude: /node_modules/,
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+// };
+
+//use webpack-dev-server
 module.exports = {
-  entry: './minus.js',
-  output: { path: __dirname, filename: 'bundle.js' },
+  entry: './app/minus.js',
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  },
   module: {
     loaders: [
       {
         test: /\js|jsx$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         query: {
           presets: ['es2015', 'react']
         }
